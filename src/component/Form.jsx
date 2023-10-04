6ae5600757b56abb376f1ae88d1b037cb384534d
Ajazkhan10 <rafiqkhan1343@gmail.com>
2023-10-04 19:07:28 +0500

    added yup and formik

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,12 +1,62 @@
+import { useEffect, useState } from "react";
+import { validateSchema } from "./schema";
+import { useFormik } from "formik";
+import clsx from "clsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Form = () => {
-  const HandleSubmit = () => {
-    console.log("hello world");
-  };
+  const [error, setError] = useState("");
+
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    touched,
+    isSubmitting,
+  } = useFormik({
+    initialValues: INITIAL_STATE,
+    validationSchema: validateSchema,
+    onSubmit: (values, { resetForm }) => {
+      if (error) {
+        setError(errors);
+      } else {
+        console.log(values);
+        setTimeout(() => {
+          resetForm();
+        }, 1000);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (errors) {
+      if (errors.email) {
+        toast(errors.email);
+      } else if (errors.password) {
+        toast(errors.password);
+      } else if (errors.confirmPassword) {
+        toast(errors.confirmPassword);
+      }
+    }
+  }, [errors]);
+
   return (
     <div className="w-full py-20">
       <div className="max-w-[500px] mx-auto py-10 bg-blue-300 px-10">
+        <ToastContainer />
         <form
-          onSubmit={HandleSubmit}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(e);
+          }}
           className="flex flex-col gap-10 font-mono font-bold"
         >
           <h1 className="text-xl text-center w-full bg-blue-600 text-white px-2 py-3 rounded-xl">
@@ -17,7 +67,13 @@ const Form = () => {
               type="email"
               name="email"
               id="email"
-              className="w-full px-2 py-3 bg-slate-100 outline-none rounded-2xl border-2 border-blue-300 hover:border-blue-700"
+              onBlur={handleBlur}
+              value={values.email}
+              onChange={handleChange}
+              className={clsx(
+                "w-full px-2 py-3 bg-slate-100 outline-none rounded-2xl border-2 border-blue-300 hover:border-blue-700",
+                touched.email && errors.email ? "border-red-500 border-2" : ""
+              )}
               placeholder="Email address.."
               required
             />
@@ -27,10 +83,18 @@ const Form = () => {
           </div>
           <div className="flex flex-col-reverse gap-5">
             <input
-              type="email"
+              type="password"
               name="password"
               id="password"
-              className="w-full px-2 py-3 bg-slate-100 outline-none rounded-2xl border-2 border-blue-300 hover:border-blue-700"
+              onBlur={handleBlur}
+              value={values.password}
+              onChange={handleChange}
+              className={clsx(
+                "w-full px-2 py-3 bg-slate-100 outline-none rounded-2xl border-2 border-blue-300 hover:border-blue-700",
+                touched.password && errors.password
+                  ? "border-red-500 border-2"
+                  : ""
+              )}
               placeholder="Password.."
               required
             />
@@ -40,23 +104,30 @@ const Form = () => {
           </div>
           <div className="flex flex-col-reverse gap-5">
             <input
-              type="email"
-              name="conform_password"
-              id="conform_password"
-              className="w-full px-2 py-3 bg-slate-100 outline-none rounded-2xl border-2 border-blue-300 hover:border-blue-700"
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              onBlur={handleBlur}
+              value={values.confirmPassword}
+              onChange={handleChange}
+              className={clsx(
+                "w-full px-2 py-3 bg-slate-100 outline-none rounded-2xl border-2 border-blue-300 hover:border-blue-700",
+                touched.confirmPassword && errors.confirmPassword
+                  ? "border-red-500 border-2"
+                  : ""
+              )}
               placeholder="Conform Password.."
               required
             />
-            <label htmlFor="conform_password" className="px-1">
+            <label htmlFor="confirmPassword" className="px-1">
               Conform Password
             </label>
           </div>
           <button
             type="submit"
-            onClick={HandleSubmit}
             className="bg-blue-600 px-10 py-3 w-fit text-white rounded-2xl hover:bg-blue-500 border-2 border-blue-300 hover:border-blue-950 "
           >
-            Submit
+            {isSubmitting ? "Loading..." : "Sign Up"}
           </button>
         </form>
       </div>
